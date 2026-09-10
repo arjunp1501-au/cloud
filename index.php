e4bc5cf0c5174ce26cf735df53182cfdbcc1bdfb
arjunp1501-au <arjun.p1501@gmail.com>
2026-09-10 09:59:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <h1>My First PHP Page</h1>
+
+    <?php
+        // This is a PHP statement that outputs text
+        echo "Hello, World!"; 
+    ?>
+
+</body>
+</html>
